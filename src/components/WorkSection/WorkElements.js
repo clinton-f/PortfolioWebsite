9c1cbc15fa52e58dcaa13512dfc50817c56246e3
Clinton F <legacy_7005@yahoo.com>
2022-07-11 15:31:55 -0400

    Main Jobs Done

--- src/components/WorkSection/WorkElements.js
+++ src/components/WorkSection/WorkElements.js
@@ -24,7 +24,8 @@ export const WorkSample1 = styled.div `
     grid-row-end: 2;
     height: 600px;
     width: 75vw;
-    background: white;
+    background: transparent;
+    position: relative;
     
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
@@ -42,21 +43,7 @@ export const WorkSample1 = styled.div `
 }
 `
 
-export const WorkImage1 = styled.div `
-    grid-column-start: 1;
-    grid-column-end: 5;
-    grid-row-start: 1;
-    grid-row-end: 7;
 
-@media screen and (max-width: 768px){
-    grid-column-start: 1;
-    grid-column-end: 7;
-    grid-row-start: 1;
-    grid-row-end: 7;
-}
-
-    background: green;
-`
 export const WorkTxt1 = styled.div `
     grid-column-start: 7;
     grid-column-end: 4;
@@ -66,6 +53,7 @@ export const WorkTxt1 = styled.div `
     text-align: right;
     align-self: center;
 
+    color: #ad7c18;
     background: none;
 
     @media screen and (max-width: 768px){
@@ -80,13 +68,31 @@ export const WorkTxt1 = styled.div `
     
 `
 
+export const WorkImage1 = styled.div `
+    grid-column-start: 1;
+    grid-column-end: 6;
+    grid-row-start: 1;
+    grid-row-end: 7;
+
+
+@media screen and (max-width: 768px){
+    grid-column-start: 1;
+    grid-column-end: 7;
+    grid-row-start: 1;
+    grid-row-end: 7;
+
+}
+
+background: transparent;
+    
+    
+`
 
 export const WorkSample2 = styled.div `
     grid-row-start: 2;
     grid-row-end: 3;
     height: 600px;
     width: 75vw;
-    background: white;
     
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
@@ -109,7 +115,6 @@ export const WorkImage2 = styled.div `
     grid-row-start: 1;
     grid-row-end: 7;
 
-    background: green;
 
     @media screen and (max-width: 768px){
     grid-column-start: 7;
@@ -125,6 +130,8 @@ export const WorkTxt2 = styled.div `
     grid-row-start: 1;
     grid-row-end: 7;
 
+    color: #ad7c18;
+
     text-align: left;
     align-self: center;
     background: none;
@@ -137,67 +144,11 @@ export const WorkTxt2 = styled.div `
 }
 `
 
-export const WorkSample3 = styled.div `
-    grid-row-start: 3;
-    grid-row-end: 4;
-    height: 600px;
-    width: 75vw;
-    background: white;
-    
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
-
-    @media screen and (max-width: 768px){
-    height: 75vh;
-    width: 90vw;
-
-}
-  
-    @media screen and (max-width: 480px){
-    height: 60vh;
-}
-`
-
-export const WorkImage3 = styled.div `
-    grid-column-start: 1;
-    grid-column-end: 5;
-    grid-row-start: 1;
-    grid-row-end: 7;
-
-    background: green;
-
-    @media screen and (max-width: 768px){
-    grid-column-start: 1;
-    grid-column-end: 7;
-    grid-row-start: 1;
-    grid-row-end: 7;
-}
-`
-
-export const WorkTxt3 = styled.div `
-    grid-column-start: 7;
-    grid-column-end: 4;
-    grid-row-start: 1;
-    grid-row-end: 7;
-    
-    text-align: right;
-    align-self: center;
-    background: none;
-
-    @media screen and (max-width: 768px){
-    grid-column-start: 1;
-    grid-column-end: 7;
-    grid-row-start: 1;
-    grid-row-end: 7;
-
-    text-align: left;
-    align-self: center;
-}
-`
 export const FeatureProject = styled.div `
+margin-bottom: 0px;
 `
 export const AppTitle = styled.div `
+margin-top: 10px;
 `
 export const AppDescription = styled.div `
 `
--- src/components/WorkSection/index.js
+++ src/components/WorkSection/index.js
@@ -1,60 +1,83 @@
 import React from 'react';
-import {WorkSectionContainer, WorkSample1, WorkSample2, WorkSample3, WorkImage1, WorkImage2, WorkImage3, WorkTxt1, WorkTxt2, WorkTxt3, FeatureProject, AppTitle, AppDescription, LanguagesUsed, ProjectFile, ProjectLink} from './WorkElements';
+import {WorkSectionContainer, WorkSample1, WorkSample2, WorkImage1, WorkImage2, WorkTxt1, WorkTxt2, FeatureProject, AppTitle, AppDescription, LanguagesUsed} from './WorkElements';
 import Fade from 'react-reveal/Fade';
+import HeroHomePageWide from '../../images/HeroHomePageWide.jpg';
+import HRHomePageWide from '../../images/HRHomePageWide.jpg';
+import FitnessDirectory from '../../images/FitnessDirectory.jpg';
+import { AiOutlineLink } from "react-icons/ai";
+import { RiGithubLine } from "react-icons/ri";
 
 const WorkSection = () => {
 
 
     return (
         <WorkSectionContainer id='Tech'>
-            <Fade bottom distance={"50%"}>  
+            <Fade bottom distance={"50%"}> 
+            
             <WorkSample1>
-                
-                <WorkImage1 className="cell"><img src="" className="sepia"/></WorkImage1>
 
-                <WorkTxt1>
-                    <FeatureProject className="featureProject">Featured Project</FeatureProject>
-                    <AppTitle className="appTitle">OctoProfile</AppTitle>
-                    <AppDescription className="appDescription">A nicer look at your GitHub profile and repository stats with data visualizations of your top languages and stars. Sort through your top repos by number of stars, forks, and size.</AppDescription>
-                    <LanguagesUsed className="languagesUsed">../C/C++/C#</LanguagesUsed>
-                    <ProjectFile className="projectFile">@</ProjectFile>
-                    <ProjectLink className="projectLink">@</ProjectLink>
+            <WorkImage1 className="cell"><img src={HeroHomePageWide} className="sepia" alt="CMS Website" /></WorkImage1>
 
-                </WorkTxt1>
+
+            <WorkTxt1 style={{position: 'relative'}}>
+                <FeatureProject className="featureProject" >Featured Project</FeatureProject>
+                <AppTitle className="appTitle"  style={{color: '#26693a'}}>Cobblestone Place</AppTitle>
+                <AppDescription className="appDescription" >C.P. is a Full-Stack Angular application that lets neighbors create events, digital business cards, and post gigs, as well as edit the front page content and more, built using tech like JWT, MongoDB, and TS.</AppDescription>
+                <LanguagesUsed className="languagesUsed" >
+                <a style={{marginRight: '20px', background: 'transparent', color: '#ad7c18', textDecorationLine: 'none'}} href="https://expressjs.com/" target="_blank">Express.js</a>
+                <a style={{marginRight: '20px', background: 'transparent', color: '#ad7c18', textDecorationLine: 'none'}} href="https://nodejs.org/en/" target="_blank">Node.js</a>
+                <a style={{marginRight: '20px', background: 'transparent', color: '#ad7c18', textDecorationLine: 'none'}} href="https://mongoosejs.com/" target="_blank">Mongoose</a>
+                <a style={{marginRight: '20px', background: 'transparent', color: '#ad7c18', textDecorationLine: 'none'}} href="https://www.npmjs.com" target="_blank">npm</a>
+                <a style={{background: 'transparent', color: '#ad7c18', textDecorationLine: 'none'}} href="https://stripe.com/" target="_blank">Stripe</a>
+                </LanguagesUsed>
+                <AiOutlineLink style={{margin: '20px'}} className="globalEffect1" size='1.6rem' onClick={()=> window.open("https://codesandbox.io/s/clinton-f", "_blank")} />
+                <RiGithubLine style={{margin: '20px 20px 20px 0px'}} className="globalEffect1" size='1.6rem' onClick={()=> window.open("https://github.com/clinton-f", "_blank")}/>
+            </WorkTxt1>
+
             </WorkSample1 >
             </Fade>  
 
             <Fade bottom> 
             <WorkSample2>
 
-                <WorkImage2 className="cell"><img src="" className="sepia"/></WorkImage2>
+                <WorkImage2 className="cell"><img src={HRHomePageWide} className="sepia2" alt="Python HR App" /></WorkImage2>
                 
-                <WorkTxt2>
+                <WorkTxt2 style={{position: 'relative'}}>
                     <FeatureProject className="featureProject">Featured Project</FeatureProject>
-                    <AppTitle className="appTitle">OctoProfile</AppTitle>
-                    <AppDescription className="appDescription">A nicer look at your GitHub profile and repository stats with data visualizations of your top languages and stars. Sort through your top repos by number of stars, forks, and size.</AppDescription>
-                    <LanguagesUsed className="languagesUsed">../C/C++/C#</LanguagesUsed>
-                    <ProjectFile className="projectFile">@</ProjectFile>
-                    <ProjectLink className="projectLink">@</ProjectLink>
+                    <AppTitle className="appTitle" style={{color: '#6b0905'}}>HR Management</AppTitle>
+                    <AppDescription className="appDescription">This Python multi-page application allows the user in an HR position to keep track of employees, add, remove, pay, or edit their information.It was built using Python bags, sets, stacks, and more.</AppDescription>
+                    <LanguagesUsed className="languagesUsed">
+                        <a style={{marginRight: '20px', background: 'transparent', color: '#ad7c18', textDecorationLine: 'none'}} href="https://streamlit.io/" target="_blank">Streamlit</a>
+                        <a style={{marginRight: '20px', background: 'transparent', color: '#ad7c18', textDecorationLine: 'none'}} href="https://pandas.pydata.org/" target="_blank">Pandas</a>
+                        <a style={{marginRight: '20px', background: 'transparent', color: '#ad7c18', textDecorationLine: 'none'}} href="https://numpy.org/" target="_blank">Numpy</a>
+                    </LanguagesUsed>
+                    <AiOutlineLink style={{margin: '20px'}} className="globalEffect1" size='1.6rem' onClick={()=> window.open("https://clinton-f-python-hr-system-app-k15f3u.streamlitapp.com/", "_blank")} />
+                    <RiGithubLine style={{margin: '20px 20px 20px 0px'}} className="globalEffect1" size='1.6rem' onClick={()=> window.open("https://github.com/clinton-f/Python-HR-System", "_blank")}/>
                 </WorkTxt2>
             </WorkSample2>
             </Fade>
 
             <Fade bottom>
-            <WorkSample3>
+            <WorkSample1>
 
-                <WorkImage3 className="cell"><img src="" className="sepia"/></WorkImage3>
+            <WorkImage1 className="cell"><img src={FitnessDirectory} className="sepia" alt="CMS Website" /></WorkImage1>
 
-                <WorkTxt3>
-                    <FeatureProject className="featureProject">Featured Project</FeatureProject>
-                    <AppTitle className="appTitle">OctoProfile</AppTitle>
-                    <AppDescription className="appDescription">A nicer look at your GitHub profile and repository stats with data visualizations of your top languages and stars. Sort through your top repos by number of stars, forks, and size.</AppDescription>
-                    <LanguagesUsed className="languagesUsed">../C/C++/C#</LanguagesUsed>
-                    <ProjectFile className="projectFile">@</ProjectFile>
-                    <ProjectLink className="projectLink">@</ProjectLink>
-                </WorkTxt3>
 
-            </WorkSample3>
+            <WorkTxt1 style={{position: 'relative'}}>
+                <FeatureProject className="featureProject" >Featured Project</FeatureProject>
+                <AppTitle className="appTitle"  style={{color: '#FF2625'}}>Fitness Directory</AppTitle>
+                <AppDescription className="appDescription" >Fitness Directory is a React.js website I built using a workout API, JavaScript, and more where you can search for more than 1000 workouts you can do both at home or at the gym.</AppDescription>
+                <LanguagesUsed className="languagesUsed" >
+                <a style={{marginRight: '20px', background: 'transparent', color: '#ad7c18', textDecorationLine: 'none'}} href="https://rapidapi.com/justin-WFnsXH_t6/api/exercisedb/details" target="_blank">ExerciseDB-API</a>
+                <a style={{marginRight: '20px', background: 'transparent', color: '#ad7c18', textDecorationLine: 'none'}} href="https://github.com/heroku" target="_blank">Heroku</a>
+                <a style={{marginRight: '20px', background: 'transparent', color: '#ad7c18', textDecorationLine: 'none'}} href="https://www.npmjs.com/package/react-router-dom" target="_blank">React-Router-DOM</a>
+                <a style={{ background: 'transparent', color: '#ad7c18', textDecorationLine: 'none'}} href="https://mui.com/" target="_blank">MUI</a>
+                </LanguagesUsed>
+                <AiOutlineLink style={{margin: '20px'}} className="globalEffect1" size='1.6rem' onClick={()=> window.open("https://fitnessdirectory.netlify.app", "_blank")} />
+                <RiGithubLine style={{margin: '20px 20px 20px 0px'}} className="globalEffect1" size='1.6rem' onClick={()=> window.open("https://github.com/clinton-f/react_fitness_directory_website", "_blank")}/>
+            </WorkTxt1>
+
+            </WorkSample1 >
             </Fade>
 
         </WorkSectionContainer>

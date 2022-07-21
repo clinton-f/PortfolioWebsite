180875d8dba5323f28944f1616d6b63504280a20
Clinton F <legacy_7005@yahoo.com>
2022-07-21 01:23:28 -0400

    last pront update

--- src/components/AboutSection/Prompt.js
+++ src/components/AboutSection/Prompt.js
@@ -1,4 +1,4 @@
 import React from 'react';
-const Prompt = () => <span id="userTag">clintonfabian7@gmail.com:~$ </span>;
+const Prompt = () => <span id="userTag">clintonfabian7@gmail.com: </span>;
 
 export default Prompt;

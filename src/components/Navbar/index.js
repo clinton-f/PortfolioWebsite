f2196ed0d5290d4ceaab49f585b15bf41b2111e1
Mr. Roboto <52689415+clinton-f@users.noreply.github.com>
2024-01-31 14:23:10 -0500

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -46,7 +46,7 @@ const toggleHome = () => {
                           <NavLinks className="globalEffect1" to="Contact" smooth={true} duration={600} /*spy={true}*/ exact="true" offset={-1}>CONTACT</NavLinks>
                       </NavItem>
                   </NavMenu>
-                  <NavBtnLink to='/' style={{margin: '20px'}} onClick={()=> window.open("https://www.dropbox.com/scl/fo/1aq04akq76yas7llhmwvj/h?rlkey=kczl013qdj04m69wec5xvbx22&dl=0", "_blank")}>RESUME</NavBtnLink>
+                  <NavBtnLink to='/' style={{margin: '20px'}} onClick={()=> window.open("https://www.dropbox.com/scl/fo/1aq04akq76yas7llhmwvj/h?dl=0&preview=WebResume.docx&rlkey=kczl013qdj04m69wec5xvbx22", "_blank")}>RESUME</NavBtnLink>
               </NavbarContainer>
           </Nav>
         </>
